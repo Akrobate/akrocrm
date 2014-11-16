be52d582b916a4946c95c5c57a7a5d5ebe0cd6a2
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-11-16 18:47:07 +0000

    ajout liste enrichi pour futurs controles sur les listes

---
+++ trunk/core/controller/list/frameview.php
@@ -0,0 +1,17 @@
+<?php
+
+class List_Frameview extends CoreController {
+
+	public function init() {
+
+		$list = new List_View();
+		$modules = $this->getModule();
+		CoreController::share($this, $list);
+		$listContent = $list->renderSTR();
+		$this->assign('list', $listContent);		
+		
+		
+		$this->assign('mainmodule', ucfirst($this->getModule()));		
+	}
+		
+}
--- trunk/core/controller/module/index.php
+++ trunk/core/controller/module/index.php
@@ -7,12 +7,21 @@ class Module_Index extends CoreController {
 		$id = request::get('id');
 		$modules = $this->getModule();
 		
+		/*
 		$list = new List_View();
 		CoreController::share($this, $list);
 		$listContent = $list->renderSTR();
+		*/
+
+		$list = new List_Frameview();
+		CoreController::share($this, $list);
+		$listContent = $list->renderSTR();
+
 
 		$this->assign('listContent', $listContent);
-		$this->assign('mainmodule', ucfirst($this->getModule()));		
+		
+		
+		
 
 	}
 }
