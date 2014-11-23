a77fbc5f395304b2b0682936e7fc19720ea1febc
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-11-23 01:17:16 +0000

    ajout de la mecanique de customisation

--- trunk/api.php
+++ trunk/api.php
@@ -3,14 +3,18 @@
 	define ("PATH_SEP", '/');	
 
 	define ("PATH_CURRENT", "." . PATH_SEP );
-	define ("PATH_CONFIGS", PATH_CURRENT. "config" . PATH_SEP);
+	define ("PATH_CONFIGS", PATH_CURRENT. "config" . PATH_SEP);	
 	define ("PATH_LIBS", PATH_CURRENT . "libs" . PATH_SEP );
 	define ("LIBS_PATH", PATH_CURRENT . "libs" . PATH_SEP );
 	define ("PATH_FONTS", PATH_CURRENT."public/fonts/");
 	define ("PATH_MODULES", PATH_CURRENT . "modules/" );
 	define ("PATH_CORE", PATH_CURRENT . "core/" );
 	define ("PATH_CORE_CONTROLLER", PATH_CORE . "controller/" );	
-	define ("PATH_CORE_VIEWS", PATH_CORE . "views/" );		
+	define ("PATH_CORE_VIEWS", PATH_CORE . "views/" );
+	define ("PATH_CUSTOM", PATH_CURRENT. "custom" . PATH_SEP);	
+	define ("PATH_CUSTOM_CONTROLLER", PATH_CUSTOM . "controller" . PATH_SEP);	
+	define ("PATH_CUSTOM_VIEWS", PATH_CUSTOM . "views". PATH_SEP);		
+
 
 	include(PATH_CONFIGS . "db.php");
 	include(PATH_LIBS . "sql.class.php");
