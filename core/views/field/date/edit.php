2d08c689acf8968f4be65df330f31862a1117754
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-11-23 14:47:59 +0000

    ajout amelioration date et gestion des ajout notes par sms (exemple de customisation)

---
+++ core/views/field/date/edit.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+	<label><?=$this->getLabel();?></label>
+	<input 
+		data-provide="datepicker"
+		class="form-control datepicker" 
+		name="<?=$this->getName()?>" 
+		value="<?=$this->getValue()?>" />
+</div>
