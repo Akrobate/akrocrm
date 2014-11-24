77ae5c6880f981103f04a384609d8274daf74a46
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-11-24 00:43:02 +0000

    fix enregisterment de nouveaux records

--- core/views/list/frameview.php
+++ core/views/list/frameview.php
@@ -4,12 +4,25 @@
 	</h2>
 </div>
 
-<form method="post" action="<?=url::internal($this->getModule(),'index', null, '&start='.$start)?>">
-	<p class="text-right">
-		<button type="submit" name="commande" value="prev" class="btn btn-default glyphicon glyphicon-chevron-left btn-xs"></button>
-		<button type="button" class="btn btn-default glyphicon btn-xs disabled"><?=$start?>/<?=$total?></button>
-		<button type="submit" name="commande" value="next" class="btn btn-default glyphicon glyphicon-chevron-right btn-xs"></button>
-	</p>
-</form>
+<div class="row">
+
+	<div class="col-md-6">
+		<p class="text-left">
+			<a href="<?=url::internal($this->getModule(),'edit')?>" type="button" class="btn btn-default glyphicon glyphicon glyphicon-new-window btn-xs"> Créer</a>
+		<p>
+		
+	</div>	
+	<div class="col-md-6">
+	
+
+		<form method="post" action="<?=url::internal($this->getModule(),'index', null, '&start='.$start)?>">
+			<p class="text-right">
+				<button type="submit" name="commande" value="prev" class="btn btn-default glyphicon glyphicon-chevron-left btn-xs"></button>
+				<button type="button" class="btn btn-default glyphicon btn-xs disabled"><?=$start?>/<?=$total?></button>
+				<button type="submit" name="commande" value="next" class="btn btn-default glyphicon glyphicon-chevron-right btn-xs"></button>
+			</p>
+		</form>
+	</div>
+</div>
 
 <?=$list ?>
