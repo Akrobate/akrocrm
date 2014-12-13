596c3a721d9c6ede1f62ba2a8789d7e8ed6c62d1
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-12-13 18:50:17 +0000

    fix

--- libs/OrmNode.class.php
+++ libs/OrmNode.class.php
@@ -2,11 +2,11 @@
 
 class OrmNode extends DataAdapter {
 
+	
 	public static $joins = array();
 	// @TODO Time a finaliser (format dentrée et icone up/down
-//	public static $allowedfields = array('text', 'join', 'largetext', 'photourl', 'date', 'time');
+	//	public static $allowedfields = array('text', 'join', 'largetext', 'photourl', 'date', 'time');
 
-	public static $allowedfields = array('text', 'join', 'largetext', 'photourl', 'date');
 
 	public $filter;
 	
@@ -51,67 +51,7 @@ class OrmNode extends DataAdapter {
 		return $allFields;
 	}
 
-	/**
-	 * 
-	 */
 
-	public static function dataFieldsAdapter($data, $fieldslist, $fieldaction = 'view', $rendered = false){
-		$ret = array();
-		foreach($data as $field => $value) {
-			if (isset($fieldslist[$field])) {
-				$type = $fieldslist[$field]['type'];
-			
-				if (in_array($type, self::$allowedfields)) {
-					$typename = ucfirst($type);
-					$classname = "Field_".$typename;
-					$obj = new $classname();
-				} else {
-					$obj = new Field_Text();
-				}
-			
-				$obj->setAllFieldsParams($field, $fieldslist[$field]);
-				$obj->setValue($value);	
-				$obj->setAction($fieldaction);
-				if ($rendered == 'rendered') {
-					$ret[$field] = $obj->renderSTR();
-				} else {
-					$ret[$field] = $obj;
-				}
-			}
-		}
-		return $ret;	
-	}
-	
-	
-	public static function dataFieldsAdapterEmpty($fieldslist, $fieldaction = 'view', $rendered = false){
-		$ret = array();
-		
-		foreach($fieldslist as $field => $val) {
-		
-			$type = $fieldslist[$field]['type'];
-			
-			if (in_array($type, self::$allowedfields)) {
-				$typename = ucfirst($type);
-				$classname = "Field_".$typename;
-				$obj = new $classname();
-			} else {
-				$obj = new Field_Text();
-			}
-			$obj->setAllFieldsParams($field, $fieldslist[$field]);
-			$obj->setValue("");	
-			$obj->setAction($fieldaction);
-	
-		
-			if ($rendered == 'rendered') {
-				$ret[$field] = $obj->renderSTR();
-			} else {
-				$ret[$field] = $obj;
-			}
-		}
-		return $ret;	
-	}	
-	
-	
 	
 
 
