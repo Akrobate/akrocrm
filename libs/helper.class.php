d6c1dade2bfc562882e4fa6f8354fe4cc49af60c
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-12-13 22:26:39 +0000

    nettoyage code et optimisation de la maintenabilité

--- libs/helper.class.php
+++ libs/helper.class.php
@@ -1,79 +1,85 @@
 <?php
 
+/**
+ * @brief		Classe url permettant de réaliser des traitement sur les urls
+ * @details		Conversions d'url, redirections, centralisation des appels aux modules          
+ * @author		Artiom FEDOROV
+ */
+
+
 class url {
 
 	static $rewrited = false;
-
-
-
+	
 	static function setRewrite($bool) {
 		self::$rewrited = $bool;
 	}
+
+	/**
+	 * @brief		Réalise une redirection header 301
+	 * @details		Peut etre appelé depuis le controlleur          
+	 * @param	controller		Nom du controlleur verss lequel on redirige
+	 * @param	action			Nom de l'action visée
+	 * @param	id				id (optionnel)
+	 * @return    null
+	 */
 
 	static function redirect($controller, $action, $id = null, $other_params = null){
 		$querystr = self::internal($controller, $action, $id, $other_params);
 		header("Location: index.php" . $querystr);
 	}
 
+	/**
+	 * @brief		Genere une url
+	 * @details		Genere l'url String en fonction des parametres d'entreé
+	 * @param	controller		Nom du controlleur verss lequel on redirige
+	 * @param	action			Nom de l'action visée
+	 * @param	id				id (optionnel)
+	 * @param	other_params	parametres optionnels, concatenation a la fin de l'url
+	 * @return    null
+	 */
+
 	static function internal($controller, $action, $id = null, $other_params = null) {
 		if (self::$rewrited) {
-			
 			$query = "$controller/$action";
 			if ($id) {
 				$query .= "/$id";
 			}
-			
 			if ($other_params) {
 				$query .= "?" . $other_params;
-			}
-			
-			
+			}	
 		} else {
 			$query = "?controller=$controller&action=$action";
 			if ($id) {
 				$query .= "&id=$id";
 			}
-			
 			if ($other_params) {
 				$query .= $other_params;
 			}
 		}
-	
 		return $query;
 	}
 
-
 }
 
 
-/*
-class link {
-
-	static function internal ($controller, $action, $id = null) {
-		
-	
-	
-	}
-
-
-
-}
-
-*/
+/**
+ * @brief		Classe str permettant de réaliser des traitement sur les chaines de carracteres
+ * @details		Conversions d'url, redirections, centralisation des appels aux modules          
+ * @author		Artiom FEDOROV
+ */
 
 class str {
 
-	static function utf8e ($str) {
-		
+	/**
+	 * @brief		utf8e encode en UTF8 une chaine de carractere
+	 * @details		Surcharge de la methode utf8_encode
+	 * @param	str			Chaine a encoder en urf8
+	 * @return    String	renvoi la chaine de carracteres encodée en utf8
+	 */
+	 
+	static function utf8e ($str) {	
 		return utf8_encode($str);
-	
 	}
 
-
-
 }
-
-
-
-
-?>
