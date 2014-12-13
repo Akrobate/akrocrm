f201cffa06f900c59f2eee6f4a4140699e3c79d9
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-12-13 18:45:47 +0000

    split de orm node avec une classe a part dataAdapter inspiré de android adaptateur de liste

---
+++ libs/dataAdapter.class.php
@@ -0,0 +1,72 @@
+<?
+
+
+class DataAdapter {
+
+
+	/**
+	 * 
+	 */
+
+	public static function dataFieldsAdapter($data, $fieldslist, $fieldaction = 'view', $rendered = false){
+		$ret = array();
+		foreach($data as $field => $value) {
+			if (isset($fieldslist[$field])) {
+				$type = $fieldslist[$field]['type'];
+			
+				if (in_array($type, self::$allowedfields)) {
+					$typename = ucfirst($type);
+					$classname = "Field_".$typename;
+					$obj = new $classname();
+				} else {
+					$obj = new Field_Text();
+				}
+			
+				$obj->setAllFieldsParams($field, $fieldslist[$field]);
+				$obj->setValue($value);	
+				$obj->setAction($fieldaction);
+				if ($rendered == 'rendered') {
+					$ret[$field] = $obj->renderSTR();
+				} else {
+					$ret[$field] = $obj;
+				}
+			}
+		}
+		return $ret;	
+	}
+	
+	
+	public static function dataFieldsAdapterEmpty($fieldslist, $fieldaction = 'view', $rendered = false){
+		$ret = array();
+		
+		foreach($fieldslist as $field => $val) {
+		
+			$type = $fieldslist[$field]['type'];
+			
+			if (in_array($type, self::$allowedfields)) {
+				$typename = ucfirst($type);
+				$classname = "Field_".$typename;
+				$obj = new $classname();
+			} else {
+				$obj = new Field_Text();
+			}
+			$obj->setAllFieldsParams($field, $fieldslist[$field]);
+			$obj->setValue("");	
+			$obj->setAction($fieldaction);
+	
+		
+			if ($rendered == 'rendered') {
+				$ret[$field] = $obj->renderSTR();
+			} else {
+				$ret[$field] = $obj;
+			}
+		}
+		return $ret;	
+	}	
+	
+	
+
+
+
+
+}
