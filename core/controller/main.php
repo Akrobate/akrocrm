1874e9c78a170439143ba9f025e9e54613c80322
fedorov.artiom <fedorov.artiom@cfa73af3-87ca-1ac6-2ff4-9a6749dbabf5>
2014-12-20 23:03:22 +0000

    des ameliorations diverses

--- core/controller/main.php
+++ core/controller/main.php
@@ -2,16 +2,27 @@
 
 class Main extends CoreController {
 
+
 	public function __construct() {
+
 		$this->template = "core/views/main.php";
+		
 	}
+
+
 	
 	public function init() {
-		$content = $this->getvar('content');
-		$contentSTR = $content->renderSTR();
-		$this->assign('contentSTR', $contentSTR);
-		$headers = ressources::$js;
-		$this->assign('headers', $headers);
+	
+		if ($this->format == "angular") {
+			$this->template = "core/views/mainAngular.php";
+		} else {
+			$this->template = "core/views/main.php";
+			$content = $this->getvar('content');
+			$contentSTR = $content->renderSTR();
+			$this->assign('contentSTR', $contentSTR);
+			$headers = ressources::$js;
+			$this->assign('headers', $headers);
+		}
 	}
 	
 }
